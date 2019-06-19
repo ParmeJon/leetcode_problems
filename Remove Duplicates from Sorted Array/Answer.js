d1c5f083d56ef0c256caeb81aa673aaed35b64b7
github username <jonathansychan@gmail.com>
2019-06-19 15:38:22 -0400

    more

---
+++ Remove Duplicates from Sorted Array/Answer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    for (let i = 1; i < nums.length; i++){
+        if (nums[i-1] === nums[i]) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+};
+var removeDuplicates = function(nums) {
+    // let unique = new Set(nums)
+    nums.splice(0, nums.length, ...(new Set(nums) ))
+};
