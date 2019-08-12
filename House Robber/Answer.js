a9bf904390ce9d030e58c77cc4669acd1d74902d
github username <jonathansychan@gmail.com>
2019-08-12 18:38:59 -0400

    More

---
+++ House Robber/Answer.js
@@ -0,0 +1,13 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+var rob = function(nums) {
+  //     Permutate indexes
+  //     0, 1, 2, 3, 4
+  //     0, 2, 4
+  //     0, 3
+  //     1, 3
+  //     1, 4
+  //     Then choose the max sum of all those permutations
+};
