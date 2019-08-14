644a3c97151208fbebc5db71344370a905017c7b
github username <jonathansychan@gmail.com>
2019-08-13 20:42:44 -0400

    House Robber

--- House Robber/Answer.js
+++ House Robber/Answer.js
@@ -11,3 +11,23 @@ var rob = function(nums) {
   //     1, 4
   //     Then choose the max sum of all those permutations
 };
+
+// Bottom Up Approach
+var rob = function(nums) {
+    
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+    
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
+    
+    for (let i = 2; i < nums.length; i++) {
+        const maxAtCurrent = Math.max(nums[i] + maxAtTwoBefore, maxAtOneBefore);
+        
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
+    }
+    
+    return maxAtOneBefore;
+};
