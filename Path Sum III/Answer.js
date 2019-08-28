5e41f5116f62d025231ee1855a4da9418caa9a29
github username <jonathansychan@gmail.com>
2019-08-28 15:55:37 -0400

    PathSum3 alternative

--- Path Sum III/Answer.js
+++ Path Sum III/Answer.js
@@ -52,3 +52,27 @@ var pathSum = function(root, sum, presums = { "0": 1 }, prev = 0) {
   presums[curr]--;
   return res;
 };
+
+// Another
+function pathSum(root, sum) {
+  let result = 0;
+
+  const addToSum = function(node, subSum) {
+    if (!node) return;
+    subSum += node.val;
+    if (subSum === sum) result++;
+    addToSum(node.left, subSum);
+    addToSum(node.right, subSum);
+  };
+
+  const traverseTree = function(node) {
+    if (!node) return;
+    addToSum(node, 0);
+    traverseTree(node.left);
+    traverseTree(node.right);
+  };
+
+  traverseTree(root);
+
+  return result;
+}
