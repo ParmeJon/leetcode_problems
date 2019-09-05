64c2593c6c2c0980744f2b3abdc1e8c663feff2a
github username <jonathansychan@gmail.com>
2019-09-04 22:25:48 -0400

    More answers for LSWRC

--- Longest Substring Without Repeating Character/Answer.js
+++ Longest Substring Without Repeating Character/Answer.js
@@ -31,3 +31,50 @@ var lengthOfLongestSubstring = function(s) {
 
   return len;
 };
+
+// ONE LOOP
+// Based on indexes
+var lengthOfLongestSubstring = function(s) {
+  var sLen = s.length,
+    maxLen = 0,
+    maxStr = "",
+    tmpStr,
+    posIndex,
+    i;
+
+  for (i = 0; i < sLen; i++) {
+    tmpStr = s[i];
+    posIndex = maxStr.indexOf(tmpStr);
+
+    if (posIndex > -1) {
+      maxStr = maxStr.substring(posIndex + 1);
+    }
+
+    maxStr += tmpStr;
+    maxLen = Math.max(maxLen, maxStr.length);
+  }
+
+  return maxLen;
+};
+
+// Another way with changing start. 
+var lengthOfLongestSubstring = function(s) {
+    let start = 0
+    let obj = {}
+    let maxLen = 0
+    
+    
+    for (let i = 0; i < s.length ; i++) {
+        if (obj[s[i]] === undefined) {
+            obj[s[i]] = i
+        } else {
+            if (obj[s[i]] + 1 > start ) {
+                start = obj[s[i]] + 1
+            }
+            obj[s[i]] = i
+        }
+        maxLen = Math.max(maxLen, i+1 - start)
+    }
+    
+    return maxLen
+};
