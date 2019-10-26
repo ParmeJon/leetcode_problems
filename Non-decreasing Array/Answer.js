4a59d38fd4386d47860aa16e74dd55407d306d71
github username <jonathansychan@gmail.com>
2019-10-25 21:25:41 -0400

    Non-decreasing Array

---
+++ Non-decreasing Array/Answer.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function(nums) {
+  if (nums.length <= 1) {
+    return true;
+  }
+  let changes = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i + 1]) {
+      changes++;
+      if (nums[i - 1] > nums[i + 1] && nums[i] > nums[i + 2]) {
+        return false;
+      }
+    }
+  }
+  if (changes > 1) {
+    return false;
+  }
+  return true;
+};
